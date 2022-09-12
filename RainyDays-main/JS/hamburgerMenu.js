3eaf77b0e795c2a36aeae60254267771c5949c2a
martindyr <76110230+martindyr@users.noreply.github.com>
2022-09-12 15:18:01 +0200

    Delete RainyDays-main directory

--- RainyDays-main/JS/hamburgerMenu.js
+++
@@ -1,9 +0,0 @@
-const hMenuBtn = document.querySelector("#hamburgerMenuBtn")
-const hamburgerMenu = document.querySelector(".hamburgerMenu")
-
-hMenuBtn.addEventListener("click", toggleMenu)
-
-
-function toggleMenu() {
-    hamburgerMenu.classList.toggle('hamburgerMenu')
-}
